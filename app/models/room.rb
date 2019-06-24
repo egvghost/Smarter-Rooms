95521baf0d091464fa2a27a1549d45ab483ac4b6
egvghost <egvghost@gmail.com>
2019-06-24 16:57:04 -0300

    improvement with self.code

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,8 @@
 class Room < ApplicationRecord
     belongs_to :building
 
-    def status(room_code)
-        #conn = Faraday.new(:url => 'https://ca-3-api.mybluemix.net/api/v1/rooms')
-        #response = conn.get "#{room_code}"
-        response = Faraday.get "https://ca-3-api.mybluemix.net/api/v1/rooms/#{room_code}"
+    def status
+        response = Faraday.get "https://ca-3-api.mybluemix.net/api/v1/rooms/#{self.code}"
         response.body
     end
 end
