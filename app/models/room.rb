ff6608565bee673c5f13534878664448560f0e8c
egvghost <egvghost@gmail.com>
2019-06-23 21:01:22 -0300

    first approach for rooms status api consumption

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,10 @@
 class Room < ApplicationRecord
     belongs_to :building
+
+    def status(room_code)
+        #conn = Faraday.new(:url => 'https://ca-3-api.mybluemix.net/api/v1/rooms')
+        #response = conn.get "#{room_code}"
+        response = Faraday.get "https://ca-3-api.mybluemix.net/api/v1/rooms/#{room_code}"
+        response.body
+    end
 end
