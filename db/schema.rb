b75a624e1f99eb1d30ba37d90a93f959dcde4c1f
egvghost <egvghost@gmail.com>
2019-06-28 11:26:18 -0300

    Special features and permissions for Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_171828) do
+ActiveRecord::Schema.define(version: 2019_06_28_135202) do
 
   create_table "accessories", force: :cascade do |t|
     t.string "name"
