5ddd0c14f9249d10f373cb767cbc10179d972d14
egvghost <egvghost@gmail.com>
2019-07-09 14:51:06 -0300

    period validation enhenced

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,25 +5,21 @@ class Reservation < ApplicationRecord
   validates :valid_from, presence: true
   scope :active, -> {where("valid_to > ?", Time.current)}
   scope :inactive, -> {where("valid_to < ?", Time.current)}
-  validate :period_overlaps
   validate :period
-  validate :timeframe
+  validate :period_overlaps
+
+  def period
+    errors.add(:reservation, "'Start time' cannot be in the past.") if self.valid_from.past?
+    errors.add(:reservation, "'End time' cannot be in the past.") if !(self.valid_from < self.valid_to)
+    errors.add(:reservation, "Reservations are only allowed from Mondays to Fridays") if (self.valid_from.on_weekend?)||(self.valid_to.on_weekend?)
+    errors.add(:reservation, "Reservations are only allowed from 9am to 6pm.") if ((self.valid_from.hour < 9)||(self.valid_to.hour > 18)||(self.valid_to.hour == 18 && self.valid_to.min > 0))
+  end
 
   def period_overlaps
     is_overlapping = Reservation.active.where(room: room).any? do |r|
-      (r.valid_from..r.valid_to).overlaps?(self.valid_from + 1.minute..self.valid_to - 1.minute)
+      (r.valid_from...r.valid_to).overlaps?(self.valid_from...self.valid_to)
     end
     errors.add(:reservation, "There is an active reservation for this Room for the specified period.") if is_overlapping
-  end
-
-  def period
-    errors.add(:reservation, "Reservation Time cannot be in the past.") if self.valid_from.past?
-    errors.add(:reservation, "'End time' must be later than 'Start time'.") if !(self.valid_from < self.valid_to)
-  end
-
-  def timeframe
-    errors.add(:reservation, "Reservations are only allowed from Mondays to Fridays - 9am to 6pm.") if (self.valid_from.on_weekend?)||(self.valid_to.on_weekend?)
-    errors.add(:reservation, "Reservations are only allowed from Mondays to Fridays - 9am to 6pm.") if (self.valid_from < Time.parse("9am"))||(self.valid_to > Time.parse("6pm"))
   end
   
 
